96d5f4fe232afa3f885633541000e802678c0882
Kay <kay05kay04@gmail.com>
2025-04-11 16:18:35 +0800

    Kay: mergeV1

--- experience/models.py
+++ experience/models.py
@@ -35,6 +35,51 @@ class TimelineEvent(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.event_date.strftime('%Y-%m-%d')}"
+    
+    def get_icon(self):
+        """Returns the appropriate Font Awesome icon class based on event type"""
+        icon_mapping = {
+            'exhibition': 'fas fa-museum',
+            'competition': 'fas fa-trophy',
+            'workshop': 'fas fa-chalkboard-teacher'
+        }
+        return icon_mapping.get(self.event_type, 'fas fa-calendar')  # default icon
+
+    def get_colors(self):
+        """Returns the color variables based on event type"""
+        color_mapping = {
+            'exhibition': {
+                'bg': 'var(--color1)',    # Purple
+                'text': 'var(--color2)'    # Light purple
+            },
+            'competition': {
+                'bg': 'var(--color4)',     # Blue
+                'text': 'var(--color3)'    # Light blue
+            },
+            'workshop': {
+                'bg': 'var(--color5)',     # Green
+                'text': 'var(--color6)'    # Light green
+            }
+        }
+        return color_mapping.get(self.event_type, {'bg': 'var(--color1)', 'text': 'var(--color2)'})
+
+    def get_color(self):
+        """Returns the color scheme based on event type"""
+        color_mapping = {
+            'exhibition': {
+                'bg': 'var(--color1)',  # Purple
+                'text': 'var(--color2)', # Light purple
+            },
+            'competition': {
+                'bg': 'var(--color4)',   # Blue
+                'text': 'var(--color3)',  # Light blue
+            },
+            'workshop': {
+                'bg': 'var(--color5)',    # Green
+                'text': 'var(--color6)',   # Light green
+            }
+        }
+        return color_mapping.get(self.event_type, {'bg': '#6c757d', 'text': '#e9ecef'})
 
     class Meta:
         ordering = ['event_date']
