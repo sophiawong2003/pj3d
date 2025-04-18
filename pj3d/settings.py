a7202e6b9773dc4a1deb1c64e9876a718a29b1ec
Kay-hktest <kay05kay04@gmail.com>
2025-04-19 03:26:19 +0800

    Kay:updated all

--- pj3d/settings.py
+++ pj3d/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'pj3d/static')]
 
 # Default primary key field type
